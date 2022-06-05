68c7ebe097460cf854f0d9b984d32612b5111b7e
link2mountain <linhtn@vnpay.vn>
2022-06-05 17:04:46 +0700

    tesssts

---
+++ src/main/java/com/demo/springcloudconfig/config/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.demo.springcloudconfig.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by linhtn on 6/4/2022.
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/encrypt");
+    }
+}
