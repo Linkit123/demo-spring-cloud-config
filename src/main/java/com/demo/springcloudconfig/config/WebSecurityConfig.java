526319e86fee3851002cbf41de299899a1b683f1
linkit@1196 <trinhngoclinh0308@gmail.com>
2022-06-05 21:22:08 +0700

    update ttt

--- src/main/java/com/demo/springcloudconfig/config/WebSecurityConfig.java
+++ src/main/java/com/demo/springcloudconfig/config/WebSecurityConfig.java
@@ -1,19 +1,29 @@
 package com.demo.springcloudconfig.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 /**
  * Created by linhtn on 6/4/2022.
  */
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 
-    @Override
-    public void configure(WebSecurity web) {
-        web.ignoring().antMatchers("/encrypt");
+    @Bean
+    @Order(0)
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .requestMatchers((matchers) -> matchers.antMatchers("/decrypt"))
+                .authorizeHttpRequests((authorize) -> authorize.anyRequest().permitAll())
+                .requestCache().disable()
+                .securityContext().disable()
+                .sessionManagement().disable();
+
+        return http.build();
     }
 }
